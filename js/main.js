34d56f0511ad2f718df6779a25630780cfd4efe9
mohannadprogrammer <waheedmohannad55578@gmail.com>
2025-09-26 22:25:42 +0200

    add spotlight

--- js/main.js
+++ js/main.js
@@ -31,3 +31,8 @@ function navaction(name) {
 
 
 }
+document.addEventListener('mousemove', function(e) {
+    const spotlight = document.querySelector('.spotlight');
+    spotlight.style.left = e.clientX + 'px';
+    spotlight.style.top = e.clientY + 'px';
+});
